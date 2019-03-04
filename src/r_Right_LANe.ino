ce828925ef0b89ae268c2cefc53b6976f9eb9998
Lan Sovinc <36709435+sovaCode@users.noreply.github.com>
2019-03-04 01:40:05 +0100

    Add files via upload

---
+++ src/r_Right_LANe.ino
@@ -0,0 +1,156 @@
+//  ____  _       _     _     _        _    _   _        ____    ___  
+// |  _ \(_) __ _| |__ | |_  | |      / \  | \ | | ___  |___ \  / _ \ 
+// | |_) | |/ _` | '_ \| __| | |     / _ \ |  \| |/ _ \   __) || | | |
+// |  _ <| | (_| | | | | |_  | |___ / ___ \| |\  |  __/  / __/ | |_| |
+// |_| \_\_|\__, |_| |_|\__| |_____/_/   \_\_| \_|\___| |_____(_)___/ 
+//          |___/                                                     
+//Made by Lan Sovinc for Right LANe MARK 2 electric longboard
+
+/*SERIAL PRINT SWITCH_____________________________________________________________________________________________________________________*/
+#define Sprintln(a) //(Serial.println(a))
+#define Sprint(b) //(Serial.print(b))
+
+/*LIBRARIES_______________________________________________________________________________________________________________________________*/
+#include <SPI.h>
+#include <RF24.h>
+#include <PWM.h>
+
+// RF24-related
+RF24 radio(7,8);
+const byte address1[6] = "00001";
+
+// Data structure for transmission
+struct Data{
+  int joystickValue;
+  bool frontLight;
+  bool backLight;
+}data;
+
+// Battery level of the skateboard
+int skateBattery = 0;
+
+// PWM-related
+int32_t frequency = 50;
+int pwmOut = 10;
+int neutral = 4915;
+long joystickValuePWM = neutral;
+
+// Lights-related
+int brightnessFront = 255;
+int brightnessBack = 128;
+int brightnessBreak = 191;
+
+// Timeout-related
+unsigned long previousTime = 0;
+const long timeout = 500;
+
+/*SETUP FUNCTION__________________________________________________________________________________________________________________________*/
+void setup() {
+
+  Serial.begin(115200);
+  Sprintln("Setting up");
+
+  // IO pins
+  pinMode(pwmOut, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+
+  // Radio initialization
+  radio.begin();
+  radio.enableAckPayload(); // Allow optional ack payloads
+  radio.enableDynamicPayloads();  // Ack payloads are dynamic payloads
+  radio.setPALevel(RF24_PA_MAX);
+  radio.openReadingPipe(1,address1);
+  radio.startListening();
+
+  // PWM initialization
+  InitTimersSafe();
+  bool success = SetPinFrequencySafe(pwmOut, frequency);
+  if (success){
+    Sprintln("Custom PWM library initialized");
+    Sprint("PWM frequency = ");
+    Sprintln(Timer1_GetFrequency());
+    Sprint("resolution: ");
+  //Serial.println(Timer1_GetTop());
+  }
+
+  // Set PWM so the board won't move
+  pwmWriteHR(pwmOut, neutral);
+
+}
+
+/*LOOP FUNCTION___________________________________________________________________________________________________________________________*/
+void loop() {
+  byte pipeNo;
+
+  // Read all available payloads
+  if( radio.available(&pipeNo)){
+    //Sprint("PipeNo is: ");
+    //Sprintln(pipeNo);
+    radio.read( &data, sizeof(data) );
+
+    // Check if the commands recived are correct
+    if(((data.joystickValue>=0)&&(data.joystickValue<=1023))&&((data.backLight==0)||(data.backLight==1))&&((data.backLight==0)||(data.backLight==1))){
+      //Execute the commands recived-----
+      
+      // Set PWM according to the joystick value
+      joystickValuePWM = map(data.joystickValue, 0, 1023, 3277, 6554);
+      pwmWriteHR(pwmOut, joystickValuePWM);
+
+      // Turn on break/back/front light
+      lightsControl();
+
+      Sprint("Recieved joystick value: ");
+      Sprintln(joystickValuePWM); 
+      Sprint("Recieved front light: ");
+      Sprintln(data.frontLight); 
+      Sprint("Recieved back light: ");
+      Sprintln(data.backLight);
+
+      previousTime = millis();
+
+      //-----
+    }
+    else{      
+      Sprint("Incorrect data");
+    }
+  
+    // Read skateboard battery voltage and send it as ACK
+    skateBattery = analogRead(A3);
+    radio.writeAckPayload(pipeNo,&skateBattery, 1 );
+    Sprint("Sending battery voltage level ");
+    Sprintln(skateBattery);
+    Sprintln();
+  }
+  else{
+    // If connection is lost, stop the skateboard and turn off the lights
+    if (millis() - previousTime >= timeout) {
+      Sprintln("Transmission failed. Resetting parameters...");
+      pwmWriteHR(pwmOut, neutral);
+      analogWrite(3, 0);
+      analogWrite(5, 0);
+      radio.stopListening();
+      delay(500);
+      radio.startListening();
+      previousTime = millis();
+    }
+  }
+}
+
+/*LIGHTS CONTROL FUNCTION_________________________________________________________________________________________________________________*/
+void lightsControl(){
+  
+  if(joystickValuePWM < neutral){
+      analogWrite(5, brightnessBreak);
+  }
+  else{
+    // Toggle back light
+    analogWrite(5, data.backLight ? brightnessBack : 0);  
+  }
+  // Toggle front light
+  analogWrite(3, data.frontLight ? brightnessFront : 0);
+}
+
+//TODO:
+//pravi pipe/channel
+
---
+++ src/t_Right_LANe.ino
@@ -0,0 +1,207 @@
+//  ____  _       _     _     _        _    _   _        ____    ___  
+// |  _ \(_) __ _| |__ | |_  | |      / \  | \ | | ___  |___ \  / _ \ 
+// | |_) | |/ _` | '_ \| __| | |     / _ \ |  \| |/ _ \   __) || | | |
+// |  _ <| | (_| | | | | |_  | |___ / ___ \| |\  |  __/  / __/ | |_| |
+// |_| \_\_|\__, |_| |_|\__| |_____/_/   \_\_| \_|\___| |_____(_)___/ 
+//          |___/                                                     
+//Made by Lan Sovinc for Right LANe MARK 2 electric longboard
+
+/*SERIAL PRINT SWITCH (uncomment to enable printing to the Serial Monitor)________________________________________________________________*/
+#define Sprintln(a) //(Serial.println(a))
+#define Sprint(b) //(Serial.print(b))
+
+/*LIBRARIES_______________________________________________________________________________________________________________________________*/
+#include <SPI.h>
+#include "RF24.h"
+
+/*VARIABLES_______________________________________________________________________________________________________________________________*/
+// Buttons, joystick and LEDs
+int joystick = A0;
+int cButton = 5;
+int zButton = 2;
+int red = 3;
+int orange = 4;
+int yellow = 6;
+int green = 9;
+int notConnected = 10;
+volatile bool zButtonReleased = true;
+bool cButtonState = 0;
+bool cButtonStatePrevious = 0;
+int neutral = 512;
+int currentThrottle = neutral;
+
+// RF24-related
+RF24 radio(7,8);
+const byte address1[6] = "00001";
+
+// Data structure for transmission
+struct Data{
+  int joystickValue;
+  bool frontLight;
+  bool backLight;
+}data;
+
+// Battery level of the skateboard
+int skateBattery = 0;
+
+// Timer-related
+unsigned long previousMillis = 0;
+const long interval = 20;
+
+/*SETUP FUNCTION__________________________________________________________________________________________________________________________*/
+void setup() {
+
+  Serial.begin(115200);
+  Sprintln("Setting up");
+
+  // IO pins
+  pinMode(cButton, INPUT);
+  pinMode(zButton, INPUT);
+  
+  // Radio initialization
+  radio.begin();
+  radio.enableAckPayload(); // Allow optional ack payloads
+  radio.enableDynamicPayloads();  // Ack payloads are dynamic payloads
+  radio.setPALevel(RF24_PA_MAX);
+  radio.openWritingPipe(address1);
+  radio.stopListening();
+
+  // Interrupt setup
+  attachInterrupt(digitalPinToInterrupt(2), setNeutral, FALLING);
+
+  // LEDs
+  pinMode(red, OUTPUT);
+  pinMode(orange, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(notConnected, OUTPUT);
+
+  // Set notConnected-LED
+  digitalWrite(notConnected, HIGH);
+
+  // Default buttons and joystick values
+  data.joystickValue = neutral;
+  data.frontLight = false;
+  data.backLight = false;
+
+}
+
+/*LOOP FUNCTION___________________________________________________________________________________________________________________________*/
+void loop() {
+
+  unsigned long currentMillis = millis();
+  if (millis() - previousMillis >= interval) {
+    previousMillis = millis();
+
+    // Read remote control input buttons and joystick
+    readThrottle();
+    readLightButton();
+      
+    // Transmit the data struct to the other radio
+    if (radio.write(&data,sizeof(data))){
+  
+      Sprint("Sent joystick value: ");
+      Sprintln(data.joystickValue); 
+      Sprint("Sent front light: ");
+      Sprintln(data.frontLight); 
+      Sprint("Sent back light: ");
+      Sprintln(data.backLight);
+      Sprintln();
+      
+      // Recieve ACK with skate battery level      
+      while(radio.available() ){
+        // Read ACK and save skateboard battery voltage
+        radio.read( &skateBattery, 1 );
+        // Display battery voltage on the remote
+        readBattery();
+        Sprint("Received skateboard battery level: ");
+        Sprintln(skateBattery);
+        Sprintln();
+        
+        // Reset notConnected-LED
+        digitalWrite(notConnected, LOW);
+      }  
+    }
+    // If no ACK response, sending failed
+    else{
+      // Reset battery indicator LEDs and set notConnected-LED
+      digitalWrite(red, LOW);
+      digitalWrite(orange, LOW);
+      digitalWrite(yellow, LOW);
+      digitalWrite(green, LOW);
+      digitalWrite(notConnected, HIGH);
+      Sprintln("Sending failed.");
+    }
+  }
+}
+
+/*READ THROTTLE JOYSTICK FUNCTION_________________________________________________________________________________________________________*/
+void readThrottle(){
+  // If safety button is pressed read the throttle value
+  if(digitalRead(zButton) == HIGH){
+    currentThrottle = analogRead(joystick);
+    if(!zButtonReleased || (currentThrottle > 512 && currentThrottle < 530)){ // the statement forces the user to press the safety button before pushing the throttle joystick
+      zButtonReleased = false;
+      data.joystickValue = analogRead(joystick);
+    }
+  }
+}
+
+/*RELEASED SAFETY BUTTON (Z) INTERRUPT FUNCTION___________________________________________________________________________________________*/
+void setNeutral(){
+  data.joystickValue = neutral;
+  zButtonReleased = true;
+  
+}
+
+/*READ LIGHT CONTROL BUTTON (C) FUNCTION__________________________________________________________________________________________________*/
+void readLightButton(){
+  cButtonState = digitalRead(cButton);
+  if(cButtonState != cButtonStatePrevious){
+    if(cButtonState == HIGH){
+      if((!data.frontLight)&&(!data.backLight)){
+        data.backLight = !data.backLight;  
+      }
+      else if((!data.frontLight)&&(data.backLight)){
+        data.frontLight = !data.frontLight;  
+      }
+      else{
+        data.backLight = !data.backLight;
+        data.frontLight = !data.frontLight;
+      }
+    }
+  }
+  cButtonStatePrevious = cButtonState;
+}
+
+/*READ SKATEBOARD BATTERY VOLTAGE FUNCTION________________________________________________________________________________________________*/
+void readBattery(){
+
+  // More than 40V
+  if (skateBattery > 925){
+    digitalWrite(red, LOW);
+    digitalWrite(orange, LOW);
+    digitalWrite(yellow, LOW);
+    digitalWrite(green, HIGH);
+  }
+  // More than 37V
+  else if (skateBattery > 855){
+    digitalWrite(red, LOW);
+    digitalWrite(orange, LOW);
+    digitalWrite(yellow, HIGH);
+    digitalWrite(green, LOW);
+  }
+  // More than 35V
+  else if (skateBattery > 805){
+    digitalWrite(red, LOW);
+    digitalWrite(orange, HIGH);
+    digitalWrite(yellow, LOW);
+    digitalWrite(green, LOW);
+  }
+  else{
+    digitalWrite(red, HIGH);
+    digitalWrite(orange, LOW);
+    digitalWrite(yellow, LOW);
+    digitalWrite(green, LOW);
+  }
+}
